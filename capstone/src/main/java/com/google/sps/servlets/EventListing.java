40807a4c6fbc4a6e94cd4f24ef3e38aec4a9dc76
Silas <benfieldsilas@gmail.com>
2020-07-09 19:34:20 +0000

    Push changes to see current code

--- capstone/src/main/java/com/google/sps/servlets/EventListing.java
+++ capstone/src/main/java/com/google/sps/servlets/EventListing.java
@@ -1,5 +1,24 @@
 package com.google.sps.servlets;
 
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.Events;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
 import java.io.IOException;
 import java.lang.UnsupportedOperationException;
 import java.util.ArrayList;
@@ -9,14 +28,97 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.security.GeneralSecurityException;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
 
 /** Servlet that returns some example content. */
 @WebServlet("/eventlisting")
 public class EventListing extends HttpServlet {
+  private static final String APPLICATION_NAME = "Google Calendar API Test";
+  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static final String TOKENS_DIRECTORY_PATH = "tokens";
+  private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
+  private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Not implemented yet
-    throw new UnsupportedOperationException("doPost is not implemented yet");
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    try {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        .setApplicationName(APPLICATION_NAME)
+        .build();
+    
+        Event event = new Event()
+            .setSummary("Google I/O 2015")
+            .setLocation("800 Howard St., San Francisco, CA 94103")
+            .setDescription("A chance to hear more about Google's developer products.");
+
+        DateTime startDateTime = new DateTime("2020-07-9T09:00:00-07:00");
+        EventDateTime start = new EventDateTime()
+            .setDateTime(startDateTime)
+            .setTimeZone("America/Los_Angeles");
+        event.setStart(start);
+
+        DateTime endDateTime = new DateTime("2020-07-9T17:00:00-07:00");
+        EventDateTime end = new EventDateTime()
+            .setDateTime(endDateTime)
+            .setTimeZone("America/Los_Angeles");
+        event.setEnd(end);
+
+        EventAttendee[] attendees = new EventAttendee[] {
+            new EventAttendee().setEmail("sbenfield@google.com"),
+        };
+        event.setAttendees(Arrays.asList(attendees));
+
+        String calendarId = "primary";
+        event = service.events().insert(calendarId, event).execute();
+        System.out.printf("Event created: %s\n", event.getHtmlLink());
+
+        // List the next 10 events from the primary calendar.
+        // DateTime now = new DateTime(System.currentTimeMillis());
+        // Events events = service.events().list("primary")
+        //         .setMaxResults(10)
+        //         .setTimeMin(now)
+        //         .setOrderBy("startTime")
+        //         .setSingleEvents(true)
+        //         .execute();
+        // List<Event> items = events.getItems();
+        // if (items.isEmpty()) {
+        //     System.out.println("No upcoming events found.");
+        // } else {
+        //     System.out.println("Upcoming events");
+        //     for (Event event : items) {
+        //         DateTime start = event.getStart().getDateTime();
+        //         if (start == null) {
+        //             start = event.getStart().getDate();
+        //         }
+        //         System.out.printf("%s (%s)\n", event.getSummary(), start);
+        //     }
+        // }
+    } catch (GeneralSecurityException e) {
+        throw new IOException("FAILED");
+    }
   }
+
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+      // Load client secrets.
+        InputStream in = EventListing.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
 }
