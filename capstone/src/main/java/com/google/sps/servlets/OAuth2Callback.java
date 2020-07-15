bc0306e254ffabeaaf9ba2a79d071a4ed7a1611a
Silas <benfieldsilas@gmail.com>
2020-07-15 16:52:43 +0000

    Restructure oAuth process

---
+++ capstone/src/main/java/com/google/sps/servlets/OAuth2Callback.java
@@ -0,0 +1,45 @@
+package com.google.sps.servlets;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.auth.oauth2.AuthorizationCodeResponseUrl;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.appengine.auth.oauth2.AbstractAppEngineAuthorizationCodeCallbackServlet;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** HTTP servlet to process access granted from user. */
+@WebServlet("/oauth2callback")
+public class OAuth2Callback extends AbstractAppEngineAuthorizationCodeCallbackServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, Credential credential)
+      throws ServletException, IOException {
+    resp.sendRedirect("/auth");
+  }
+
+  @Override
+  protected void onError(
+      HttpServletRequest req, HttpServletResponse resp, AuthorizationCodeResponseUrl errorResponse)
+      throws ServletException, IOException {
+    String nickname = UserServiceFactory.getUserService().getCurrentUser().getNickname();
+    resp.getWriter().print("<h3>" + nickname + ", Error");
+    resp.setStatus(200);
+    resp.addHeader("Content-Type", "text/html");
+  }
+
+  @Override
+  protected String getRedirectUri(HttpServletRequest req) throws ServletException, IOException {
+    return Utils.getRedirectUri(req);
+  }
+
+  @Override
+  protected AuthorizationCodeFlow initializeFlow() throws IOException {
+    return Utils.newFlow();
+  }
+}
