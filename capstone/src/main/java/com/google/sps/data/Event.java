6bea8d10babc3639f98e9ca07172f73ac1a1c0a8
Dera <chideraoemeonye@gmail.com>
2020-07-21 21:39:51 +0000

    Made Changes

--- capstone/src/main/java/com/google/sps/data/Event.java
+++ capstone/src/main/java/com/google/sps/data/Event.java
@@ -1,3 +1,5 @@
+package com.google.sps.data;
+
 public class Event{
     String id; 
     String location;
@@ -6,23 +8,23 @@ public class Event{
     public void EventSummary(String summary, String id) {
         this.summary = summary;
         this.id = id;
-
     }
 
-     public void Event(String summary, String id, String location) {
+    public void Event(String summary, String id, String location) {
         this.summary = summary;
         this.id = id;
         this.location = location;
-
     }
+
     public String getid() {
         return id;
     }
+
     public String getlocation() {
         return location;
     }
+
     public String getsummary() {
         return summary;
-    }
-   
+    } 
 }
