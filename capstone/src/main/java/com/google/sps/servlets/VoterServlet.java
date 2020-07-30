5a9350916a80fad553b66e9d03e6f44294fe3407
Silas <benfieldsilas@gmail.com>
2020-07-30 20:22:05 +0000

    Edit env directory

--- capstone/src/main/java/com/google/sps/servlets/VoterServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/VoterServlet.java
@@ -17,7 +17,7 @@ import org.json.*;
 
 @WebServlet("/VoterServlet")
 public class VoterServlet extends HttpServlet {
-
+ 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
